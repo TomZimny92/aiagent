ebcf79ea4b49492e487c685b6f8533415f3145c5
TomZimny92 <tzimny92@gmail.com>
2025-07-12 20:56:04 -0400

    calculator part

--- main.py
+++ main.py
@@ -9,17 +9,23 @@ def main():
         load_dotenv()
         api_key = os.environ.get("GEMINI_API_KEY")
         client = genai.Client(api_key=api_key)
+
+        contents = sys.argv[1]
         messages = [
-            types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+            types.Content(role="user", parts=[types.Part(text=contents)]),
         ]
         #contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-        contents = sys.argv[1]
+        
         response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
         prompt_tokens = response.usage_metadata.prompt_token_count
         response_tokens = response.usage_metadata.candidates_token_count
+        if len(sys.argv) > 2:
+            output_level = sys.argv[2]
+            if output_level == "--verbose":
+                print(f"User prompt: {contents}")
+                print(f"Prompt tokens: {prompt_tokens}")
+                print(f"Response tokens: {response_tokens}")
         print(response.text)
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
     else:
         print("no argument was provided")
         sys.exit(1)
