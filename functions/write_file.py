0b9f37f59f9bce35b0457b53854ecd77de913de9
TomZimny92 <tzimny92@gmail.com>
2025-08-12 15:02:50 -0400

    trying to write

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,16 +1,29 @@
 import os
+from pathlib import Path
 from config import *
 
 def write_file(working_directory, file_path, content):
-    full_path = os.path.join(working_directory, file_path)
+    
     try:
+        while file_path.startswith('/') or file_path.startswith('.'):
+            file_path = file_path[1:]
+        full_path = os.path.join(working_directory, file_path)
         working_dir_abs_path = os.path.abspath(working_directory)
+        #print(f"working_dir_abs_path: {working_dir_abs_path}")
+        #file_abs_path = os.path.abspath(file_path)
         full_abs_path = os.path.abspath(full_path)
-        if not full_abs_path.startswith(working_dir_abs_path):
+        #print(f"full_abs_path: {full_abs_path}")
+        #print(f"workdirabs: {working_dir_abs_path}")
+        #print(f"file_abs_path: {file_abs_path}")
+        fp = Path(full_abs_path)
+        print(f"fp.name: {fp.name}")
+        print(f"fp.parent: {fp.parent}")
+        #print(f"commonpath: {os.path.commonpath([full_abs_path, working_dir_abs_path])}")
+        if os.path.commonpath([full_abs_path, working_dir_abs_path]) != working_dir_abs_path:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-        if not os.path.exists(file_path):
-            os.makedirs(file_path)
-        with open(file_path, "w") as f:
+        if not os.path.exists(fp.parent):
+            os.makedirs(fp.parent)
+        with open(fp.name, "w") as f:
             f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
